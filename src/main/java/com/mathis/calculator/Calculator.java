140ad74e263c6be8d51505c0a9ea1ece56745f72
Rivrais Mathis <mrnowakowski@iutbayonne.univ-pau.fr>
2025-09-25 10:02:06 +0200

    add basic arithmetic operations and corresponding tests

--- src/main/java/com/mathis/calculator/Calculator.java
+++ src/main/java/com/mathis/calculator/Calculator.java
@@ -1,4 +1,16 @@
 package com.mathis.calculator;
 
 public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
 }
--- src/test/java/com/mathis/calculator/CalculatorTest.java
+++ src/test/java/com/mathis/calculator/CalculatorTest.java
@@ -1,7 +1,19 @@
 package com.mathis.calculator;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Test;
 class CalculatorTest {
+    @Test
+    void testAdd() {
+        Calculator calc = new Calculator();
+        assertEquals(5, calc.add(2, 3));
+        assertEquals(0, calc.add(-2, 2));
+    }
 
+    @Test
+    void testDivide() {
+        Calculator calc = new Calculator();
+        assertEquals(2, calc.divide(6, 3));
+        assertEquals(0, calc.divide(0, 5));
+    }
 }
