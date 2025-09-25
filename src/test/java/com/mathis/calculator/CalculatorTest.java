74c89f1c6677fed820c5f1867e89f7c8d6e74e47
Rivrais Mathis <mrnowakowski@iutbayonne.univ-pau.fr>
2025-09-25 10:08:56 +0200

    add parameterized test for addition in CalculatorTest

--- src/test/java/com/mathis/calculator/CalculatorTest.java
+++ src/test/java/com/mathis/calculator/CalculatorTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 
 class CalculatorTest {
@@ -17,5 +19,18 @@ class CalculatorTest {
     void testDivide() {
         assertEquals(2, Calculator.divide(6, 3));
         assertEquals(0, Calculator.divide(0, 5));
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1, 1",
+            "1, 2, 3",
+            "-2, 2, 0",
+            "0, 0, 0",
+            "-1, -2, -3"
+    })
+    void testAddParametre(int opG, int opD, int resultatAttendu) {
+        assertEquals(resultatAttendu, Calculator.add(opG, opD));
     }
 }
